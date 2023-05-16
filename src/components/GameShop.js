6d908f68f7d76b06e4720338e302afab2d77524d
joafjell <jafjell@hiof.no>
2023-05-17 00:33:53 +0200

    prøver meg frem
    testet litt mapping for å se hvordan vi skal finne fram informasjonen

--- src/components/GameShop.js
+++ src/components/GameShop.js
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom";
 import {store} from "./Games";
+import GameCard from "./GameCard";
 
-export default function GameShop({title, released, genres, img}){
+export default function GameShop(){
     return(
         <>
         <section>
-            <h1>Game Shop</h1>
+            <h2>Game Shop</h2>
             <ul>
                 {store?.map((game, index) =>{
                     const {Title, Released, Genres, Img} = game
-                    return (title={Title} released={Released})
-
+                    console.log(game)
+                    return <GameCard key={index} title={Title} released={Released} genres={Genres} img={Img} slug={game?.game?.label.replace(/\s/g, "-").toLowerCase()}/>
+                    
                 })}            
             </ul>    
         </section>
