2aecf955f427a174a58b69b071363a7b016430ed
joafjell <jafjell@hiof.no>
2023-05-18 16:23:20 +0200

    fikset småtteri

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Layout from './components/Layout';
 import GameShop from './components/GameShop';
 import { store, mygames } from './components/Games';
 import MyFavourites from './components/MyFavourites';
-
+import GamePage from './components/GamePage';
+import MyGames from './components/MyGames';
 /*har brukt https://github.com/joafjell/uin23ak4_moviesearch_fjell/blob/main/src/App.js som inspirasjon*/
 
 
@@ -16,6 +17,8 @@ function App() {
           <Route index element={<Dashboard/>}/>
           <Route path='/GameShop' element={<GameShop store={store}/>}/>
           <Route path='/MyFavourites' element={<MyFavourites mygames={mygames}/>}/>
+          <Route path='/GamePage' element={<GamePage mygames={mygames}/>}/>
+          <Route path='/MyGames' element={<MyGames mygames={mygames}/>}/>
         </Route>
       </Routes>
     );
