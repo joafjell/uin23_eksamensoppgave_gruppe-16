a939dd1194027e1b984abd652cfdf66953a03c85
joafjell <jafjell@hiof.no>
2023-05-19 09:11:34 +0200

    fikset css og responsivitet

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,12 +5,12 @@ import GameTile from "./GameTile";
 
 export default function Dashboard(){ 
     const randomGames = store.sort(() => 0.5 - Math.random()) //Brukte koden på denne siden https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj som inspirasjon
-    const shortGameList = randomGames.slice(0, 3) //Bruke en kode på denne siden https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array som inspirasjon
+    const shortGameList = randomGames.slice(0, 3) //Brukte en kode på denne siden https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array som inspirasjon
     
     const favourite = mygames?.filter((favourite) => favourite.fav === true)//her ble https://www.w3schools.com/jsref/jsref_filter.asp brukt for å forstå .filter metoden også ble .filter metoden fra https://github.com/ackarlse/news/blob/main/src/components/Main.js brukt som inspirasjon
-    const shortenedFavourites = favourite.slice(0, 2) //Bruke en kode på denne siden https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array som inspirasjon
+    const shortenedFavourites = favourite.slice(0, 2) //Brukte en kode på denne siden https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array som inspirasjon
 
-    const libraryGames = mygames.slice(0, 4) //Bruke en kode på denne siden https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array som inspirasjon
+    const libraryGames = mygames.slice(0, 4) //Brukte en kode på denne siden https://stackoverflow.com/questions/34883068/how-to-get-first-n-number-of-elements-from-an-array som inspirasjon
 
     return ( 
         <>
